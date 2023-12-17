8ba047e05ebb1cd8d49114e19ec2a796520c6d1b
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2023-12-17 18:58:50 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 5154364
+let accountEmail = "abhishek@gmail.accountEmail"
+var accountPassword = "#abhishek@123"
+accountCity = "Bhagalpur"
+let accountName
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountName]);
+
+/* 
+
+  *var use not used in modern days because it has issue in block scope and funcitional scope 
+  
+  *const can not be changed 
+  *let and var can be updated 
+  *if variable is initialised then it gives undefined output 
+
+*/
