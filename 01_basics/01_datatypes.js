e45a3bd28d87cf190d498f12df6e4bd52e21c3ca
Abhishek Gupta <133193177+ashhek@users.noreply.github.com>
2023-12-17 20:51:40 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,15 @@
+/* There is 8 type of date type
+  7 premitive:- 
+     1.number (2^53)
+     2.bigint
+     3.string ""
+     4.boolean
+     5.null (standalone value)
+     6.undefined
+     7.symbol (unique)
+
+   1 non premitive:-
+     8.object  (can store collection of data unlike premitive that can only store single date type)
+
+     //for further :  https://javascript.info/types#:~:text=There%20are%208%20basic%20data,integer%20numbers%20of%20arbitrary%20length.
+     */
